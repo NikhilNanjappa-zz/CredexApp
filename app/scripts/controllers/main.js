fd951fecce2d8f4f51fbd033bf3cf81f0da64439
Nikhil Nanjappa <nikhilnanjappa@Nikhils-MacBook-Pro.local>
2015-04-22 12:23:02 +0530

    BS modal to ngularUI modal

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the panthaProfileApp
  */
 angular.module('panthaProfileApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('MainCtrl', function ($scope, $modal) {
+
+  // fires on click of contact button
+  $scope.contact = function () {
+    $modal.open({
+      templateUrl: 'views/contactModal.html',
+      controller: function ($scope, $modalInstance) {
+
+			  $scope.ok = function () {
+			    $modalInstance.dismiss('cancel');
+			  };
+
+			  $scope.cancel = function () {
+			    $modalInstance.dismiss('cancel');
+			  };
+      }
+    });
+  };
+
+});
